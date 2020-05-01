43919d5f3451f3c208d75a6f637a7b63f8d77857
slamet rianto <slametrianto25@gmail.com>
2020-05-01 23:29:29 +0700

    dicoding web

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <h1 class="judul">Dicoding</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </div>
+
+<!-- </div class="hero"></div> -->
+
+<img src="img/ero.png" class="hero" width="800" height="400">
+
+<div class="content cf">
+<div class="main">
+   <h2>Judul Article</h2>
+<p class="penulis">ditulis oleh <a href="#">Slamet Rianto</a> .pada 1 mei 2020</p>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni esse sint tempore dolorem, impedit non vero sequi, odio illo deserunt rem illum nemo. Reprehenderit, sed at. Necessitatibus adipisci magni corrupti.</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate blanditiis eos doloribus molestias, animi ducimus quasi voluptatem error odit aliquam. Aut, voluptatem. Nihil, voluptatem quasi ullam totam possimus quisquam assumenda itaque odio consectetur minus impedit voluptatibus, rem qui obcaecati placeat veritatis unde. Obcaecati impedit ipsa tenetur asperiores repellendus. Ipsum rerum molestias voluptatum quidem ad nisi error eaque maxime odit eligendi iusto rem sint tempore, consequatur dicta. Minima ea voluptatum dolor sint ducimus laboriosam asperiores officiis modi. Officia deserunt quas error! Dignissimos amet magni quasi explicabo alias, repellendus sit quis quos recusandae libero corporis quaerat optio eum nihil rerum doloribus deleniti!</p>
+
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non natus veritatis ratione, ipsam quibusdam temporibus aspernatur consequuntur quasi, asperiores ullam odio error cumque rerum facilis voluptate recusandae dolorum vel quae perferendis distinctio beatae quis numquam. Rerum beatae aspernatur odit ab eum doloremque ex nam, voluptatem culpa nihil fugit dolores ratione iste natus sunt perferendis, unde pariatur tenetur fugiat. Sed facilis aperiam et quae ratione maiores molestiae ipsam perspiciatis consectetur ab.</p>
+</div>
+
+<aside class="sidebar">
+    <h3>About</h3>
+    <img src="img/slamet.jpg">
+    <p> slamet rianto</p>
+    <p>kp.rawa bambu rt03/02 no.38 kel.kalibaru kec.medan satria bekasi barat 17138</p>
+    <p>Junior Programmer Web</p>
+</aside>
+
+</div>
+
+<div class="footer">
+    <p class="copy">Copyright 2020. slamet rianto</p>
+</div>
+</div>
+
+    
+</body>
+</html>
